f97abfac9b4a3c7b8b11340461a8d7eb00afaaef
Amaya Nethmini <awedamulla00@gmail.com>
2023-03-07 08:44:49 +0530

    new nav-bar

--- customersupport1/public/nav-bar.php
+++ customersupport1/public/nav-bar.php
@@ -1,87 +1,62 @@
-<?php
-
-include '../config/connection.php';
-
-
-//session_start();
-   
-// if(isset($_SESSION['id']) && isset($_SESSION['email'])) {
-//   //header('Location:../public/home.php');   //why these?
-//       $firstname = $_SESSION['fname'];
-//       $lastname  = $_SESSION['lname'];
-//       $pro_pic   = $_SESSION['pro_pic'];
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-<title>Home</title>
-<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-<link rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script
-  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
-</script>
-<link rel="stylesheet" href="./css/home.css">
-</head>
-<body>
-<div class="nav-bar" style="top: 8px;">
-
-<ul>
-  <!-- <div class="nav-bar-icons" class="active"> -->
-    <li class="list-item">
-      <a class="active" href="./home.php">
-      <i class='bx bx-grid-alt' style='color:#8d8da7'></i>Dashboard</a>
-    </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./team.php"><i class='bx bx-group'></i>Team</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./reviews.php"><i class='bx bx-bookmark-heart'></i>Reviews</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./calendar-tmp.php"><i class='bx bx-calendar'></i>Calendar</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./notification-store.php"><i class='bx bx-envelope'></i>Notification Store</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./submit-a-blog-article.php"><i class='bx bx-edit-alt' style='color:#8d8da7' ></i>Write a blog</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./csmmessageportal.php"><i class='bx bx-message-rounded-dots'></i>Message Portal</a>
-  </li>
-  <!-- </div> -->
-  <!-- <div class="nav-bar-icons"> -->
-  <li>
-    <a href="./loginFront.php" id="log_out"><i class='bx bx-log-out'></i>Logout</a>
-  </li>
-  <!-- </div> -->
-</ul>
-</div>
-
-</body>
+    <head>
+        <title>Navigation Bar</title>
+        <link rel="stylesheet" href="./css/navbar.css">
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li>
+                    <a class= "active" href="./home.php">
+                        <div class="ico"><i class='bx bx-grid-alt'></i></div>
+                        <div class="icon_name">Dashboard</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./team.php">
+                        <div class="ico"><i class='bx bx-group'></i></div>
+                        <div class="icon_name">Team</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./reviews.php">
+                        <div class="ico"><i class='bx bx-bookmark-heart'></i></div>
+                        <div class="icon_name">Reviews</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./messages.php">
+                        <div class="ico"><i class='bx bx-message'></i></div>
+                        <div class="icon_name">Customer Feedback</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./notification-store.php">
+                        <div class="ico"><i class='bx bx-envelope'></i></div>
+                        <div class="icon_name">Notifications Store</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./submit-a-blog-article.php">
+                        <div class="ico"><i class='bx bx-edit-alt'></i></div>
+                        <div class="icon_name">Write to blog</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="./csmmessageportal.php">
+                        <div class="ico"><i class='bx bx-message-rounded-dots'></i></div>
+                        <div class="icon_name">Message</div>
+                    </a>
+                </li><br /><br /><br /><br /><br /><br /><br /><br />
+                <li>
+                    <a href="../src/logout.php">
+                        <div class="ico"><i class='bx bx-log-out bx-rotate-180' ></i></div>
+                        <div class="icon_name">Logout</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </body>
 </html>
-
-<?php
-
-// }else {
-// //   header("Location: ../public/loginFront.php");
-//   exit();
-// }
-
-?>
