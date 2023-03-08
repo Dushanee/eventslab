fe36a19e20ee4fba01fbc5a55ea44cd6796037d9
Amaya Nethmini <awedamulla00@gmail.com>
2023-03-08 14:09:00 +0530

    dashboard updated.

--- customersupport1/public/home.php
+++ customersupport1/public/home.php
@@ -10,6 +10,7 @@ session_start();
                              $_SESSION['lname'];
                              $_SESSION['pro_pic'];
                              
+                             
 
 ?>
 
@@ -22,6 +23,10 @@ session_start();
 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="./css/calendar.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../css/calendar.css">
+<script src="../js/calendar.js" defer></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script
   src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
   
@@ -92,143 +97,12 @@ session_start();
       </div>
     </div>
   </div>
-  <!-- <a href="./csmprofile.php" class="go" style="text-decoration: none;">
-  <div class="card2">
-    <div class="img">
-      <img src="./images/propic.png">
-    </div> -->
-    <!-- <div class="top-text">
-      <div class="name0" style="margin-top: 10px;"><p style="padding:5px;"><p>
-        See your profile
 
-    </p>
-    </div> -->
-    <!-- <script type="text/javascript" src="./calendar.js"></script> -->
-    <div id="calendar" class="calendar">
-  <div class="calendar-title">
-    <div class="calendar-title-text"></div>
-    <div class="calendar-button-group">
-      <button id="prevMonth">&lt;</button>
-      <button id="today">Today</button>
-      <button id="nextMonth">&gt;</button>
-    </div>
-  </div>
-  <div class="calendar-day-name"></div>
-  <div class="calendar-dates"></div>
-</div>
+  <?php include './html/calendar.html';?>
 
-<script>
-  let currentDate = dayjs();
-let daysInMonth = dayjs().daysInMonth();
-let firstDayPosition = dayjs().startOf("month").day();
-let monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-let weekNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-let dateElement = document.querySelector("#calendar .calendar-dates");
-let calendarTitle = document.querySelector(".calendar-title-text");
-let nextMonthButton = document.querySelector("#nextMonth");
-let prevMonthButton = document.querySelector("#prevMonth");
-let dayNamesElement = document.querySelector(".calendar-day-name");
-let todayButton = document.querySelector("#today");
-let dateItems = null;
-let newMonth = null;
 
-weekNames.forEach(function (item) {
-  dayNamesElement.innerHTML += `<div>${item}</div>`;
-});
-
-function plotDays() {
-  let count = 1;
-  dateElement.innerHTML = "";
-
-  let prevMonthLastDate = currentDate.subtract(1, "month").endOf("month").$D;
-  let prevMonthDateArray = [];
-
-  //plot prev month array
-  for (let p = 1; p < firstDayPosition; p++) {
-    prevMonthDateArray.push(prevMonthLastDate--);
-  }
-  prevMonthDateArray.reverse().forEach(function (day) {
-    dateElement.innerHTML += `<button class="calendar-dates-day-empty">${day}</button>`;
-  });
-
-  //plot current month dates
-  for (let i = 0; i < daysInMonth; i++) {
-    dateElement.innerHTML += `<button class="calendar-dates-day">${count++}</button>`;
-  }
-
-  //next month dates
-  let diff =
-    42 - Number(document.querySelector(".calendar-dates").children.length);
-  let nextMonthDates = 1;
-  for (let d = 0; d < diff; d++) {
-    document.querySelector(
-      ".calendar-dates"
-    ).innerHTML += `<button class="calendar-dates-day-empty">${nextMonthDates++}</button>`;
-  }
-
-  //month name and year
-  calendarTitle.innerHTML = `${
-    monthNames[currentDate.month()]
-  } - ${currentDate.year()}`;
-}
-
-//highlight current date
-function highlightCurrentDate() {
-  dateItems = document.querySelectorAll(".calendar-dates-day");
-  if (dateElement && dateItems[currentDate.$D - 1]) {
-    dateItems[currentDate.$D - 1].classList.add("today-date");
-  }
-}
-
-//next month button event
-nextMonthButton.addEventListener("click", function () {
-  newMonth = currentDate.add(1, "month").startOf("month");
-  setSelectedMonth();
-});
-
-//prev month button event
-prevMonthButton.addEventListener("click", function () {
-  newMonth = currentDate.subtract(1, "month").startOf("month");
-  setSelectedMonth();
-});
-
-//today button event
-todayButton.addEventListener("click", function () {
-  newMonth = dayjs();
-  setSelectedMonth();
-  setTimeout(function () {
-    highlightCurrentDate();
-  }, 50);
-});
-
-//set next and prev month
-function setSelectedMonth() {
-  daysInMonth = newMonth.daysInMonth();
-  firstDayPosition = newMonth.startOf("month").day();
-  currentDate = newMonth;
-  plotDays();
-}
-
-//init
-plotDays();
-setTimeout(function () {
-  highlightCurrentDate();
-}, 50);
-
-</script>
-
+    
+    <!-- Calendar -->
+    
 </body>
 </html>
