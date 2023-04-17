81e5ddffefc2053d8c0221b6d69c56a74aba15b1
Amaya Nethmini <awedamulla00@gmail.com>
2023-04-17 07:40:32 +0530

    Create create_faqs.php

---
+++ customersupport1/src/create_faqs.php
@@ -0,0 +1,34 @@
+<?php
+    require '../config/connection.php';
+    session_start();
+    echo 'net';
+
+    if(isset($_POST['submit'])) {
+        echo 'Amaya';
+        $_SESSION['faq_name']=$_POST['faq_name'];
+        $_SESSION['faq_answer']=$_POST['faq_answer'];
+        // echo $_SESSION['help_name'];
+
+        // $help_name=$_SESSION['help_name'];
+        $faq_name=$_SESSION['faq_name'];
+        $faq_answer=$_SESSION['faq_answer'];
+
+        $query = "INSERT INTO customer_FAQ(faq_name, faq_answer) VALUES('$faq_name', '$faq_answer')"; 
+        $result=mysqli_query($conn, $query);
+
+        if($result) {
+            echo "x";
+            header('Location: ../public/update_help_center.php?message_faq=FAQ entered successfully!');
+    }else {
+        echo "y";
+        header("Location: ../public/update_help_center.php?message_faq=Something went wrong! Please try again.");
+        exit();
+    }
+}
+
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //     echo "Form submitted";
+    // } else {
+    //     echo "Form not submitted";
+    // }
+?>
